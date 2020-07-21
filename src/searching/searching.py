82eca15ac456759b13de91b3dcb08ed5ee2e4442
Rourke Struthers <rourkestruthers@rourkes-mbp.lan>
2020-07-21 15:41:53 -0400

    Completed MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,30 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # identify the midpoint
+    mid = int((start + end) / 2)
+    
+    # check for the array size and make sure it's greater than 1 -- meaning it's
+    # not empty
+    if start >= end:
+        return -1
+    
+    # return the mid index if it is target
+    if arr[mid] == target:
+        # return mid index
+        return mid
+
+    # if the mid value is less than the target (go right side)
+    if arr[mid] < target:
+        # recursively call the function and change the start to reflect where
+        # tne midpoint is -- the end stays the same since we are on the right
+        # side of the tree
+        return binary_search(arr, target, mid, end)
+    # else: the mid value is greater than the target (go left side)
+    else:
+        # recursively call the function and change the end to reflect where
+        # tne midpoint is -- the start stays the same since we are on the left
+        # side of the tree
+        return binary_search(arr, target, start, mid)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +35,3 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
